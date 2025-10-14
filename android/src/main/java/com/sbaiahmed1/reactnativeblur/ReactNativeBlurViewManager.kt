c958f864e389c3d94a57ada53caf5f376efa9eb0
Ahmed Sbai <ahmedclubust@gmail.com>
2025-10-15 00:12:51 +0200

    feat(android): add no-op setIgnoreSafeArea prop for blur view

--- android/src/main/java/com/sbaiahmed1/reactnativeblur/ReactNativeBlurViewManager.kt
+++ android/src/main/java/com/sbaiahmed1/reactnativeblur/ReactNativeBlurViewManager.kt
@@ -63,10 +63,15 @@ class ReactNativeBlurViewManager : ViewGroupManager<ReactNativeBlurView>(),
   override fun setGlassType(view: ReactNativeBlurView?, glassType: String?) {
     view?.setGlassType(glassType ?: "clear")
   }
-  
+
   @ReactProp(name = "isInteractive")
   override fun setIsInteractive(view: ReactNativeBlurView?, isInteractive: Boolean) {
     view?.setIsInteractive(isInteractive)
+  }
+
+  @ReactProp(name = "setIgnoreSafeArea")
+  override fun setIgnoreSafeArea(view: ReactNativeBlurView?, isInteractive: Boolean) {
+    // no-op
   }
 
   /**
