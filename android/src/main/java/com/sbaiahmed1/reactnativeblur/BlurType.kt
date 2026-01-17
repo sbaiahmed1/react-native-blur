19654aebe4ced567c07d4fc8a03f785c4a2fca73
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2026-01-17 14:50:21 -0300

    fix(android): update blur types color from `QmBlurView` library

--- android/src/main/java/com/sbaiahmed1/reactnativeblur/BlurType.kt
+++ android/src/main/java/com/sbaiahmed1/reactnativeblur/BlurType.kt
@@ -9,25 +9,25 @@ import android.graphics.Color
 enum class BlurType(val overlayColor: Int) {
   XLIGHT(Color.argb(140, 240, 240, 240)),
   LIGHT(Color.argb(42, 255, 255, 255)),
-  DARK(Color.argb(120, 26, 22, 22)),
-  EXTRA_DARK(Color.argb(102, 35, 35, 35)),
+  DARK(Color.argb(120, 25, 25, 25)),
+  EXTRA_DARK(Color.argb(160, 35, 35, 35)),
   REGULAR(Color.argb(35, 255, 255, 255)),
-  PROMINENT(Color.argb(140, 240, 240, 240)),
+  PROMINENT(Color.argb(130, 240, 240, 240)),
   SYSTEM_ULTRA_THIN_MATERIAL(Color.argb(75, 240, 240, 240)),
   SYSTEM_ULTRA_THIN_MATERIAL_LIGHT(Color.argb(77, 240, 240, 240)),
   SYSTEM_ULTRA_THIN_MATERIAL_DARK(Color.argb(65, 40, 40, 40)),
   SYSTEM_THIN_MATERIAL(Color.argb(102, 240, 240, 240)),
   SYSTEM_THIN_MATERIAL_LIGHT(Color.argb(105, 240, 240, 240)),
   SYSTEM_THIN_MATERIAL_DARK(Color.argb(102, 35, 35, 35)),
-  SYSTEM_MATERIAL(Color.argb(140, 245, 245, 245)),
-  SYSTEM_MATERIAL_LIGHT(Color.argb(140, 248, 248, 248)),
+  SYSTEM_MATERIAL(Color.argb(130, 242, 242, 242)),
+  SYSTEM_MATERIAL_LIGHT(Color.argb(130, 245, 245, 245)),
   SYSTEM_MATERIAL_DARK(Color.argb(215, 65, 60, 60)),
-  SYSTEM_THICK_MATERIAL(Color.argb(210, 248, 248, 248)),
-  SYSTEM_THICK_MATERIAL_LIGHT(Color.argb(212, 248, 248, 248)),
-  SYSTEM_THICK_MATERIAL_DARK(Color.argb(165, 35, 35, 35)),
-  SYSTEM_CHROME_MATERIAL(Color.argb(165, 248, 248, 248)),
-  SYSTEM_CHROME_MATERIAL_LIGHT(Color.argb(167, 248, 248, 248)),
-  SYSTEM_CHROME_MATERIAL_DARK(Color.argb(100, 32, 32, 32));
+  SYSTEM_THICK_MATERIAL(Color.argb(160, 240, 240, 240)),
+  SYSTEM_THICK_MATERIAL_LIGHT(Color.argb(160, 242, 242, 242)),
+  SYSTEM_THICK_MATERIAL_DARK(Color.argb(160, 35, 35, 35)),
+  SYSTEM_CHROME_MATERIAL(Color.argb(135, 240, 240, 240)),
+  SYSTEM_CHROME_MATERIAL_LIGHT(Color.argb(135, 242, 242, 242)),
+  SYSTEM_CHROME_MATERIAL_DARK(Color.argb(90, 32, 32, 32));
 
   companion object {
     /**
