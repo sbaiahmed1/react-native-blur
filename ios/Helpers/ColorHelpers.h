74f83bd47cf01540313a840342f6b9f99b771e87
Oskar Eichler <62393985+OskarEichler@users.noreply.github.com>
2026-08-27 23:32:08 +0300

    refactor(ios): share and cache native color parsing

---
+++ ios/Helpers/ColorHelpers.h
@@ -0,0 +1,5 @@
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+FOUNDATION_EXPORT UIColor *RNBlurColorFromString(NSString *_Nullable colorString);
+NS_ASSUME_NONNULL_END
--- ios/ReactNativeProgressiveBlurView.mm
+++ ios/ReactNativeProgressiveBlurView.mm
@@ -1,4 +1,5 @@
 #import "ReactNativeProgressiveBlurView.h"
+#import "Helpers/ColorHelpers.h"
 
 #import <react/renderer/components/ReactNativeBlurViewSpec/ComponentDescriptors.h>
 #import <react/renderer/components/ReactNativeBlurViewSpec/EventEmitters.h>
@@ -23,112 +24,7 @@ using namespace facebook::react;
 }
 
 + (UIColor *)colorFromString:(NSString *)colorString {
-  // Input validation
-  if (!colorString || [colorString isEqualToString:@""] || colorString.length == 0) {
-    return [UIColor clearColor];
-  }
-
-  if (colorString.length > 50) {
-    NSLog(@"[ReactNativeProgressiveBlurView] Warning: Color string too long, using default clear color");
-    return [UIColor clearColor];
-  }
-
-  // Handle common color names
-  NSDictionary *colorMap = @{
-    @"black": [UIColor blackColor],
-    @"blue": [UIColor blueColor],
-    @"brown": [UIColor brownColor],
-    @"clear": [UIColor clearColor],
-    @"cyan": [UIColor cyanColor],
-    @"magenta": [UIColor magentaColor],
-    @"gray": [UIColor grayColor],
-    @"green": [UIColor greenColor],
-    @"orange": [UIColor orangeColor],
-    @"purple": [UIColor purpleColor],
-    @"red": [UIColor redColor],
-    @"transparent": [UIColor clearColor],
-    @"white": [UIColor whiteColor],
-    @"yellow": [UIColor yellowColor],
-  };
-
-  UIColor *namedColor = colorMap[colorString.lowercaseString];
-  if (namedColor) {
-    return namedColor;
-  }
-
-  // Handle hex colors
-  NSString *hexString = colorString;
-  if ([hexString hasPrefix:@"#"]) {
-    if (hexString.length < 2) {
-      NSLog(@"[ReactNativeProgressiveBlurView] Warning: Invalid hex color format '%@'", colorString);
-      return [UIColor clearColor];
-    }
-    hexString = [hexString substringFromIndex:1];
-  }
-
-  NSCharacterSet *hexCharacterSet = [NSCharacterSet characterSetWithCharactersInString:@"0123456789ABCDEFabcdef"];
-  NSCharacterSet *invalidCharacters = [hexCharacterSet invertedSet];
-  if ([hexString rangeOfCharacterFromSet:invalidCharacters].location != NSNotFound) {
-    NSLog(@"[ReactNativeProgressiveBlurView] Warning: Invalid hex color format '%@'", colorString);
-    return [UIColor clearColor];
-  }
-
-  // Handle 6-character hex (RGB)
-  if (hexString.length == 6) {
-    unsigned int hexValue;
-    NSScanner *scanner = [NSScanner scannerWithString:hexString];
-    if ([scanner scanHexInt:&hexValue] && [scanner isAtEnd]) {
-      return [UIColor colorWithRed:((hexValue & 0xFF0000) >> 16) / 255.0
-                             green:((hexValue & 0x00FF00) >> 8) / 255.0
-                              blue:(hexValue & 0x0000FF) / 255.0
-                             alpha:1.0];
-    }
-  }
-  // Handle 8-character hex (RGBA)
-  else if (hexString.length == 8) {
-    unsigned long long hexValue;
-    NSScanner *scanner = [NSScanner scannerWithString:hexString];
-    if ([scanner scanHexLongLong:&hexValue] && [scanner isAtEnd]) {
-      return [UIColor colorWithRed:((hexValue & 0xFF000000) >> 24) / 255.0
-                             green:((hexValue & 0x00FF0000) >> 16) / 255.0
-                              blue:((hexValue & 0x0000FF00) >> 8) / 255.0
-                             alpha:(hexValue & 0x000000FF) / 255.0];
-    }
-  }
-  // Handle 4-character hex (RGBA shorthand)
-  else if (hexString.length == 4) {
-    unsigned int hexValue;
-    NSScanner *scanner = [NSScanner scannerWithString:hexString];
-    if ([scanner scanHexInt:&hexValue] && [scanner isAtEnd]) {
-      // Expand 4-digit hex to 8-digit (e.g., "FFF0" -> "FFFFFF00")
-      unsigned int r = (hexValue & 0xF000) >> 12;
-      unsigned int g = (hexValue & 0x0F00) >> 8;
-      unsigned int b = (hexValue & 0x00F0) >> 4;
-      unsigned int a = (hexValue & 0x000F);
-
-      return [UIColor colorWithRed:(r | (r << 4)) / 255.0 green:(g | (g << 4)) / 255.0 blue:(b | (b << 4)) / 255.0 alpha:(a | (a << 4)) / 255.0];
-    }
-  }
-  // Handle 3-character hex (RGB shorthand)
-  else if (hexString.length == 3) {
-    unsigned int hexValue;
-    NSScanner *scanner = [NSScanner scannerWithString:hexString];
-    if ([scanner scanHexInt:&hexValue] && [scanner isAtEnd]) {
-      unsigned int r = (hexValue & 0xF00) >> 8;
-      unsigned int g = (hexValue & 0x0F0) >> 4;
-      unsigned int b = (hexValue & 0x00F);
-      return [UIColor colorWithRed:(r | (r << 4)) / 255.0
-                             green:(g | (g << 4)) / 255.0
-                              blue:(b | (b << 4)) / 255.0
-                             alpha:1.0];
-    }
-  } else {
-    NSLog(@"[ReactNativeProgressiveBlurView] Warning: Unsupported hex color length (%lu) for '%@', expected 3, 4, 6, or 8 characters",
-          (unsigned long)hexString.length, colorString);
-  }
-
-  NSLog(@"[ReactNativeProgressiveBlurView] Warning: Could not parse color '%@'", colorString);
-  return [UIColor clearColor];
+  return RNBlurColorFromString(colorString);
 }
 
 + (ComponentDescriptorProvider)componentDescriptorProvider
